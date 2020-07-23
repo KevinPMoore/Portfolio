01f084303d513d42da6818007fa362a9e4568a07
Kevin Moore <moore.kevin.p@gmail.com>
2020-07-23 15:43:12 -0400

    initial pass on header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Header from './components/header/header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
-
-export default App;
+export default class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  };
+};
---
+++ src/components/header/header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Burger from './Hamburger_icon.png';
+import Xicon from './X_icon.png';
+import './header.css';
+
+export default class Header extends React.Component {
+    state = {
+        burger: 'show',
+        buttons: 'hide'
+    };
+
+    handleToggleClick = () => {
+        if(this.state.burger === 'show') {
+            this.setState({
+                burger: 'hide',
+                buttons: 'show'
+            });
+        } else {
+            this.setState({
+                burger: 'show',
+                buttons: 'hide'
+            });
+        };
+    };
+
+    render() {
+        return(
+            <header>
+                <h3
+                    className='signature'
+                >
+                    Kevin P. Moore
+                </h3>
+                <nav>
+                    <img className={[this.state.burger, 'burgericon'].join(' ')} src={Burger} alt='a hamburger icon of three horizontle lines' onClick={this.handleToggleClick}></img>
+                    <div
+                        className={this.state.buttons}
+                    >
+                        <img className ='xicon' src={Xicon} alt='a white X' onClick={this.handleToggleClick}></img>
+                        <div
+                            className='jumper-container'
+                        >
+                            <a class="jumper" href="#about-me">About Me</a>
+                            <a class="jumper" href="#portfolio">Portfolio</a>
+                            <a href="https://github.com/KevinPMoore" target="_blank">Github</a>
+                            <a href="https://www.linkedin.com/in/kevin-p-moore/" target="_blank">LinkedIn</a>
+                            <a class="jumper" href="#contact">Contact Me</a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        );
+    };
+}

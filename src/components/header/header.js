f82f574ff5be8e594fe3d3895d981680ea537742
Kevin Moore <moore.kevin.p@gmail.com>
2020-07-29 17:41:31 -0400

    reworked header for responsive design

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -23,6 +23,18 @@ export default class Header extends React.Component {
         };
     };
 
+    renderIcons() {
+        if(this.state.burger === 'show') {
+            return(
+                <img className='burgericon' src={Burger} alt='a hamburger icon of three horizontle lines' onClick={this.handleToggleClick}></img>
+            );
+        } else {
+            return(
+                <img className ='xicon' src={Xicon} alt='a white X' onClick={this.handleToggleClick}></img>
+            );
+        }
+    }
+
     render() {
         return(
             <header>
@@ -34,48 +46,43 @@ export default class Header extends React.Component {
                     >
                         Kevin P. Moore
                     </h3>
-                    <img className={[this.state.burger, 'burgericon'].join(' ')} src={Burger} alt='a hamburger icon of three horizontle lines' onClick={this.handleToggleClick}></img>
+                    {this.renderIcons()}
                 </div>
                 <nav
                     className={this.state.buttons}
                 >
-                    <img className ='xicon' src={Xicon} alt='a white X' onClick={this.handleToggleClick}></img>
-                    <div
-                        className='link-container'
+                    <button
+                        className='nav-button'
+                        onClick={this.props.updateAbout}
                     >
+                        About Me
+                    </button>
+                    <button
+                        className='nav-button'
+                        onClick={this.props.updateProjects}
+                    >
+                        My Portfolio
+                    </button>
+                    <a  className='nav-button' href='https://github.com/KevinPMoore' target='_blank' rel='noopener noreferrer'>
                         <button
                             className='nav-button'
-                            onClick={this.props.updateAbout}
                         >
-                            About Me
+                            Github
                         </button>
+                    </a>
+                    <a  className='nav-button' href='https://www.linkedin.com/in/kevin-p-moore/' target='_blank' rel='noopener noreferrer'>
                         <button
                             className='nav-button'
-                            onClick={this.props.updateProjects}
                         >
-                            My Portfolio
+                            LinkedIn
                         </button>
-                        <a href='https://github.com/KevinPMoore' target='_blank' rel='noopener noreferrer'>
-                            <button
-                                className='nav-button'
-                            >
-                                Github
-                            </button>
-                        </a>
-                        <a href='https://www.linkedin.com/in/kevin-p-moore/' target='_blank' rel='noopener noreferrer'>
-                            <button
-                                className='nav-button'
-                            >
-                                LinkedIn
-                            </button>
-                        </a>
-                        <button
-                            className='nav-button'
-                            onClick={this.props.updateContact}
-                        >
-                            Contact Me
-                        </button>
-                    </div>
+                    </a>
+                    <button
+                        className='nav-button'
+                        onClick={this.props.updateContact}
+                    >
+                        Contact Me
+                    </button>
                 </nav>
             </header>
         );

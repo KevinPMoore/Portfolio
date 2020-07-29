753cdea688acd43002f0a6ecebc8eeef1c241f71
Kevin Moore <moore.kevin.p@gmail.com>
2020-07-29 09:10:59 -0400

    initial content pass

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Profile from './Profile.jpg';
 import './contact.css';
 
 export default class Contact extends React.Component {
@@ -6,10 +7,14 @@ export default class Contact extends React.Component {
     render() {
         return(
             <section className='contact' id='contact'>
-                <h2>Contact</h2>
+                <h2
+                    className='title'
+                >
+                    Contact
+                </h2>
                 <div className='contact-container'>
                     <div className='profile-pic'>
-                        <img className='profile' src='./images/profile.png' alt='me in front of a lake on a cloudy day'></img>
+                        <img className='profile' src={Profile} alt='me in front of a lake on a cloudy day'></img>
                     </div>
                     <div className='info'>
                         <p>Check out all my work on my <a className='contact-link' href='https://github.com/KevinPMoore' target='_blank' rel='noopener noreferrer'>Github</a> page.</p>
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import BGG from './BoardGameGuideSS.png';
+import FS from './FairShareSS.png';
+import HL from './HighLowSS.png';
+import KQ from './QuizSS.png';
 import './projects.css';
 
 export default class Projects extends React.Component {
@@ -8,7 +12,9 @@ export default class Projects extends React.Component {
                 className='portfolio' 
                 id='portfolio'
             >
-                <h2>
+                <h2
+                    className='title'
+                >
                     Project Portfolio
                 </h2>
                 <div 
@@ -18,9 +24,72 @@ export default class Projects extends React.Component {
                         className='project'
                     >
                         <h3>
+                           Fair Share
+                        </h3>
+                        <img className='screenshot' src={FS} alt='a screenshot of chore distribution'></img>
+                        <p>
+                            Manage all your household chores in one place with this simple app.  Fair Share lets users set up their own household or join
+                            an existing one.  They can then decide what needs to be done and then manually or randomly decide who will do it.
+                        </p>
+                        <p>
+                            This project was built using React, Node.js and Express with PostgreSQL for the databse.
+                        </p>
+                        <div
+                            className='link-container'
+                        >
+                            <a className='code-link' href='https://fair-share-sigma.vercel.app/' target='_blank' rel='noopener noreferrer'>Try it out</a>
+                            <a className='code-link' href='https://github.com/KevinPMoore/Fair-Share-Client' target='_blank' rel='noopener noreferrer'>See the code</a>
+                        </div>
+                    </div>
+                    <div
+                        className='project'
+                    >
+                        <h3>
+                           High-Low 
+                        </h3>
+                        <img className='screenshot' src={HL} alt='a screenshot of a wagering game'></img>
+                        <p>
+                            This lightweight game lets users push their luck as they wager points against the outcome of a random number generator.  An account 
+                            registration and login system allows users to save their progress between sessions to set a high score!
+                        </p>
+                        <p>
+                            This project was built using React, Node.js and Express with PostgreSQL for the databse.
+                        </p>
+                        <div
+                            className='link-container'
+                        >
+                            <a className='code-link' href='https://high-low.now.sh/' target='_blank' rel='noopener noreferrer'>Try it out</a>
+                            <a className='code-link' href='https://github.com/KevinPMoore/High-Low-Client' target='_blank' rel='noopener noreferrer'>See the code</a>
+                        </div>
+                    </div>
+                    <div
+                        className='project'
+                    >
+                        <h3>
+                           Board Game Guide 
+                        </h3>
+                        <img className='screenshot' src={BGG} alt='a screenshot of a board game search result'></img>
+                        <p>
+                            There's a lot more to board games than Risk or Monopoly these days, and Board Game Guide helps you sort through them all! This 
+                            app lets users explore the internet's most comprehensive board game database and find the centerpiece of their next game night.
+                        </p>
+                        <p>
+                            This project was built using HTML, Javascript and jQuery while integrating the Board Game Atlas API.
+                        </p>
+                        <div
+                            className='link-container'
+                        >
+                            <a className='code-link' href='https://kevinpmoore.github.io/Board-Game-Guide/' target='_blank' rel='noopener noreferrer'>Try it out</a>
+                            <a className='code-link' href='https://github.com/KevinPMoore/Board-Game-Guide' target='_blank' rel='noopener noreferrer'>See the code</a>
+                        </div>
+                    </div>
+                    <div
+                        className='project'
+                    >
+                        <h3>
                             Kitchen Metrics Quiz
                         </h3>
-                        <img className='quiz' src='./images/quiz.png' alt='the start screen of a quiz with cooking ingredients in the background'></img>
+                        <img className='screenshot' src={KQ} alt='a screenshot of a quiz with cooking ingredients in the background'></img>
                         <p>
                             Get handy in the kitchen using this quick quiz app to learn metric conversions! This app allows users to test their knowledge of 
                             metric conversions commonly used in cooking. Score and progress are displayed across the top of the screen.
@@ -28,22 +97,12 @@ export default class Projects extends React.Component {
                         <p>
                             This project was built using HTML, Javascript and jQuery.
                         </p>
-                        <a className='code-link' href='https://thinkful-ei-iguana.github.io/Caleb-Kevin-Quiz/' target='_blank' rel='noopener noreferrer'>Try it out</a>
-                        <a className='code-link' href='https://github.com/thinkful-ei-iguana/Caleb-Kevin-Quiz' target='_blank' rel='noopener noreferrer'>See the code</a>
-                    </div>
-                    <div 
-                        className='project'>
-                        <h3>Bookmark Manager</h3>
-                        <img className='bookmarks' src='./images/bookmarks.png' alt='a bookmark app showing expanded and collapsed bookmarks'></img>
-                        <p>
-                            A simple, intuitive application for gathering and sorting bookmarks to your favorite websites. Users can add and delete bookmakrs 
-                            to different websites, while creating rankings and short descriptions. Bookmarks can also be filtered by rating.
-                        </p>
-                        <p>
-                            This project was built using HTML, Javascript, Jquery and API interfacing.
-                        </p>
-                        <a className='code-link' href='https://thinkful-ei-iguana.github.io/Kevin-bookmarks-app/' target='_blank' rel='noopener noreferrer'>Try it out</a>
-                        <a className='code-link' href='https://github.com/thinkful-ei-iguana/Kevin-bookmarks-app' target='_blank' rel='noopener noreferrer'>See the code</a>
+                        <div
+                            className='link-container'
+                        >
+                            <a className='code-link' href='https://thinkful-ei-iguana.github.io/Caleb-Kevin-Quiz/' target='_blank' rel='noopener noreferrer'>Try it out</a>
+                            <a className='code-link' href='https://github.com/thinkful-ei-iguana/Caleb-Kevin-Quiz' target='_blank' rel='noopener noreferrer'>See the code</a>
+                        </div>
                     </div>
                 </div>
             </section>
